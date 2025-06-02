cd5a2abe54e16c3ec2f465d6b3a80fc974f49326
Victor Hugo <161888868+victaomachado@users.noreply.github.com>
2025-06-02 10:44:24 -0300

    feature: adicionar arquivos spring
    Adicionando arquivos do Spring Initialzr para dentro do repositório do projeto em questão.

---
+++ imagelite-api-model/src/main/java/com/victor/imagelite_api_model/ImageliteApiModelApplication.java
@@ -0,0 +1,13 @@
+package com.victor.imagelite_api_model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageliteApiModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageliteApiModelApplication.class, args);
+	}
+
+}
---
+++ imagelite-api-model/src/test/java/com/victor/imagelite_api_model/ImageliteApiModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.imagelite_api_model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageliteApiModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
