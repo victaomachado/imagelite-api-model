c42379c029cba58010b3abec0d1a206d3a540520
Victor Hugo <161888868+victaomachado@users.noreply.github.com>
2025-06-02 11:07:45 -0300

    feat: criar endpoint starter da application

--- imagelite-api-model/src/main/java/com/victor/imagelite_api_model/application/images/ImagesController.java
+++ imagelite-api-model/src/main/java/com/victor/imagelite_api_model/application/images/ImagesController.java
@@ -1,4 +1,9 @@
 package com.victor.imagelite_api_model.application.images;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/images")
 public class ImagesController {
 }
