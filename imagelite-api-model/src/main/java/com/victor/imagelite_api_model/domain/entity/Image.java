686bab2d1b8fcc1f762be14e1082a5c218b63d91
Victor Hugo <161888868+victaomachado@users.noreply.github.com>
2025-06-02 16:30:41 -0300

    feat: criar entity Image.java
    Criando a entidade Image para representa-lo dentro do Banco de dados.

---
+++ imagelite-api-model/src/main/java/com/victor/imagelite_api_model/domain/entity/Image.java
@@ -0,0 +1,31 @@
+package com.victor.imagelite_api_model.domain.entity;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column
+    private String name;
+    @Column
+    private Long size;
+    @Column
+    @Enumerated
+    private ImageExtension extension;
+    @Column
+    @CreatedDate
+    private LocalDateTime uploadDate;
+    @Column
+    private String tags;
+    @Column
+    @Lob
+    private byte[] file;
+}
