8eafe8ea71b1f2def102225979340f488a28006b
Victor Hugo <161888868+victaomachado@users.noreply.github.com>
2025-06-02 11:19:20 -0300

    feat: criar método POST

--- imagelite-api-model/src/main/java/com/victor/imagelite_api_model/application/images/ImagesController.java
+++ imagelite-api-model/src/main/java/com/victor/imagelite_api_model/application/images/ImagesController.java
@@ -1,9 +1,25 @@
 package com.victor.imagelite_api_model.application.images;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/images")
 public class ImagesController {
+
+    @PostMapping
+    public ResponseEntity save(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("name") String name,
+            @RequestParam("tags") List<String> tags
+            ) {
+        
+        return ResponseEntity.ok().build();
+    }
 }
