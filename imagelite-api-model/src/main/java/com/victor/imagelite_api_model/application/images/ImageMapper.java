71bc540fe4f8d08e89476f7fafb0fa6823e79317
Victor Hugo <161888868+victaomachado@users.noreply.github.com>
2025-06-02 20:13:29 -0300

    fix: some bugged files

---
+++ imagelite-api-model/src/main/java/com/victor/imagelite_api_model/application/images/ImageMapper.java
@@ -0,0 +1,25 @@
+package com.victor.imagelite_api_model.application.images;
+
+import com.victor.imagelite_api_model.domain.entity.Image;
+import com.victor.imagelite_api_model.domain.enums.ImageExtension;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import java.util.List;
+
+
+import java.io.IOException;
+
+@Component
+public class ImageMapper {
+
+    public Image mapToImage(MultipartFile file, String name, List<String> tags) throws IOException {
+        return Image.builder()
+                .name(name)
+                .tags(String.join(",", tags))
+                .size(file.getSize())
+                .extension(ImageExtension.valueOf(MediaType.valueOf(file.getContentType())))
+                .file(file.getBytes())
+                .build();
+    }
+}
--- imagelite-api-model/src/main/java/com/victor/imagelite_api_model/application/images/ImagesController.java
+++ imagelite-api-model/src/main/java/com/victor/imagelite_api_model/application/images/ImagesController.java
@@ -1,19 +1,19 @@
 package com.victor.imagelite_api_model.application.images;
 
 import com.victor.imagelite_api_model.domain.entity.Image;
-import com.victor.imagelite_api_model.domain.enums.ImageExtension;
 import com.victor.imagelite_api_model.domain.service.ImageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -23,6 +23,7 @@ import java.util.List;
 public class ImagesController {
 
     private final ImageService service;
+    private final ImageMapper mapper;
 
     @PostMapping
     public ResponseEntity save(
@@ -32,15 +33,18 @@ public class ImagesController {
             ) throws IOException {
         log.info("Imagem recebida: name: {}, size: {}", file.getOriginalFilename(), file.getSize());
 
-        Image image = Image.builder()
-                        .name(name)
-                        .tags(String.join(",", tags))
-                        .size(file.getSize())
-                        .extension(ImageExtension.valueOf(MediaType.valueOf(file.getContentType())))
-                        .file(file.getBytes())
-                        .build();
+        Image image = mapper.mapToImage(file, name, tags);
+        Image savedImage = service.save(image);
+        URI imageUri = buildImageURL(savedImage);
 
-        service.save(image);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.created(imageUri).build();
+    }
+
+    private URI buildImageURL(Image image) {
+        String imagePath = "/" + image.getId();
+        ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path(imagePath)
+                .build().toUri();
     }
 }
