b0db0524acba012a27c553199defa8b219728941
Victor Hugo <161888868+victaomachado@users.noreply.github.com>
2025-06-02 18:44:35 -0300

    test: adicionar teste
    Cuja finalidade eh identificar o objeto dentro do banco de dados.

--- imagelite-api-model/src/main/java/com/victor/imagelite_api_model/Application.java
+++ imagelite-api-model/src/main/java/com/victor/imagelite_api_model/Application.java
@@ -1,12 +1,32 @@
 package com.victor.imagelite_api_model;
 
+import com.victor.imagelite_api_model.domain.entity.Image;
+import com.victor.imagelite_api_model.domain.enums.ImageExtension;
+import com.victor.imagelite_api_model.infra.repository.ImageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class Application {
+
+	@Bean
+	public CommandLineRunner commandLineRunner(@Autowired ImageRepository repository){
+		return args -> {
+			Image image = Image
+					.builder()
+					.extension(ImageExtension.PNG)
+					.name("myimage")
+					.tags("teste")
+					.size(1000L)
+					.build();
+			repository.save(image);
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
