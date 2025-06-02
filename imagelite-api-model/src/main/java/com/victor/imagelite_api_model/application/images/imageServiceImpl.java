3fe768a01e1dea78f518605e74e4763683dae714
Victor Hugo <161888868+victaomachado@users.noreply.github.com>
2025-06-02 19:06:41 -0300

    feat: adicionar camada Service
    Adicionando a camada Service dentro do projeto.

---
+++ imagelite-api-model/src/main/java/com/victor/imagelite_api_model/application/images/imageServiceImpl.java
@@ -0,0 +1,22 @@
+package com.victor.imagelite_api_model.application.images;
+
+import com.victor.imagelite_api_model.domain.entity.Image;
+import com.victor.imagelite_api_model.domain.service.ImageService;
+import com.victor.imagelite_api_model.infra.repository.ImageRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class imageServiceImpl implements ImageService {
+
+    private final ImageRepository repository;
+
+    @Override
+    @Transactional
+    public Image save(Image image) {
+        return repository.save(image);
+    }
+
+}
---
+++ imagelite-api-model/src/main/java/com/victor/imagelite_api_model/domain/service/ImageService.java
@@ -0,0 +1,7 @@
+package com.victor.imagelite_api_model.domain.service;
+
+import com.victor.imagelite_api_model.domain.entity.Image;
+
+public interface ImageService {
+    Image save(Image image);
+}
