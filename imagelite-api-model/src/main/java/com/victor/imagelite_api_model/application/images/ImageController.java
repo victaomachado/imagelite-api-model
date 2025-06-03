e40dac4c1cf993656417903a588c5d1ac48f9433
Victor Hugo <161888868+victaomachado@users.noreply.github.com>
2025-06-02 23:09:39 -0300

    update: renomear nome ImageController.java

--- imagelite-api-model/src/main/java/com/victor/imagelite_api_model/application/images/ImageController.java
+++ imagelite-api-model/src/main/java/com/victor/imagelite_api_model/application/images/ImageController.java
@@ -20,7 +20,7 @@ import java.util.List;
 @RequestMapping("/v1/images")
 @Slf4j
 @RequiredArgsConstructor
-public class ImagesController {
+public class ImageController {
 
     private final ImageService service;
     private final ImageMapper mapper;
