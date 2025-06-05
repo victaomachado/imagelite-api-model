d8c1b00377c71ce5390358f90f937035769aba72
Victor Hugo <161888868+victaomachado@users.noreply.github.com>
2025-06-05 12:23:44 -0300

    feat: adicionar nova Interface
    Interface nomeada JpaSpecificationExecutor para realizar buscas dinamicas.

--- imagelite-api-model/src/main/java/com/victor/imagelite_api_model/infra/repository/ImageRepository.java
+++ imagelite-api-model/src/main/java/com/victor/imagelite_api_model/infra/repository/ImageRepository.java
@@ -2,6 +2,7 @@ package com.victor.imagelite_api_model.infra.repository;
 
 import com.victor.imagelite_api_model.domain.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ImageRepository extends JpaRepository<Image, String> {
+public interface ImageRepository extends JpaRepository<Image, String>, JpaSpecificationExecutor<Image> {
 }
